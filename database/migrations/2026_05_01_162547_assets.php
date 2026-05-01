99a461697bd7c24361e35b7eef2ec595d6b72de3
Keiner Mendoza <keinermendoza@gmail.com>
2026-05-01 12:43:41 -0400

    fix: add autoincrement to assets id

--- database/migrations/2026_05_01_162547_assets.php
+++ database/migrations/2026_05_01_162547_assets.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('assets', function (Blueprint $table) {
-            $table->unsignedBigInteger('id')->primary();
+            $table->unsignedBigInteger('id')->autoIncrement();
             $table->string('name');
             $table->string('path');
             $table->string('type');
