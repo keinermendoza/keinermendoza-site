5c8aa69e589b32b26642db7cd47ab36f0cc2d6fd
Keiner Mendoza <keinermendoza@gmail.com>
2026-05-01 17:46:27 -0400

    feat: add Project model, controller, index route and layout

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,26 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::prefix('dashboard')->group(function () { 
+        Route::get('/', function() {
+            return view('dashboard');
+        })->name('dashboard');
+    
+        Route::prefix('projects')->name('projects.')->group(function () { 
+            Route::get('/', [ProjectController::class, 'index'])->name('index');
+        });
+
+    });
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
