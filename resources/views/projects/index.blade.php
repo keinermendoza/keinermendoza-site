fb8bf04259a576804fb33fedf981a73e9a6b5735
Keiner Mendoza <keinermendoza@gmail.com>
2026-07-13 16:32:21 -0400

    style: modifica altura das cards de projetos e adiciona stroke branco aos textos

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -14,7 +14,7 @@
 
             <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
                 @foreach ($projects as $project)
-                    <x-home.project-card :project="$project" class="min-h-40 max-h-80" />
+                    <x-home.project-card :project="$project" class="max-h-80" />
                 @endforeach
             </div>
         </x-section-wrapper>
