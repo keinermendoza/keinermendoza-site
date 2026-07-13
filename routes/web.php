0cf57266f33b2b799b1eab8d75d57246d4caaaaa
Keiner Mendoza <keinermendoza@gmail.com>
2026-07-13 14:04:19 -0400

    fix: ajusta ordenamento por importância na página dos projetos

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::prefix('projetos')->name('projects.')->group(function() {
 
     Route::get('/', function () {
         return view('projects.index', [
-            "projects" => Project::public()->with('tags')->get(),
+            "projects" => Project::public()->orderBy('importance', 'desc')->with('tags')->get(),
         ]);
     })->name('index');
 
@@ -35,58 +35,6 @@ Route::prefix('projetos')->name('projects.')->group(function() {
     })->name('show');
 
 });
-
-
-
-// Route::middleware(['auth', 'verified', 'admin'])->group(function () {
-//     Route::prefix('dashboard')->group(function () {
-//         Route::get('/', function() {
-//             return redirect()->route('projects.index');
-//         })->name('dashboard');
-
-//         Route::prefix('projects')->name('projects.')->group(function () {
-//             Route::get('/', [ProjectController::class, 'index'])->name('index');
-//             Route::post('/', [ProjectController::class, 'store'])->name('store');
-//             Route::get('/create', [ProjectController::class, 'create'])->name('create');
-//             Route::get('/{project}', [ProjectController::class, 'edit'])->name('edit');
-//             Route::put('/{project}', [ProjectController::class, 'update'])->name('update');
-//             Route::delete('/{project}', [ProjectController::class, 'destroy'])->name('destroy');
-//         });
-
-//         Route::prefix('posts')->name('posts.')->group(function () {
-//             Route::get('/', [PostController::class, 'index'])->name('index');
-//             Route::post('/', [PostController::class, 'store'])->name('store');
-//             Route::get('/create', [PostController::class, 'create'])->name('create');
-//             Route::get('/{post}', [PostController::class, 'edit'])->name('edit');
-//             Route::put('/{post}', [PostController::class, 'update'])->name('update');
-//             Route::delete('/{post}', [PostController::class, 'destroy'])->name('destroy');
-//         });
-
-//         Route::prefix('tags')->name('tags.')->group(function () {
-//             Route::get('/', [TagController::class, 'index'])->name('index');
-//             Route::post('/', [TagController::class, 'store'])->name('store');
-//             Route::get('/create', [TagController::class, 'create'])->name('create');
-//             Route::get('/{tag}', [TagController::class, 'edit'])->name('edit');
-//             Route::put('/{tag}', [TagController::class, 'update'])->name('update');
-//             Route::delete('/{tag}', [TagController::class, 'destroy'])->name('destroy');
-//         });
-
-//         Route::prefix('assets')->name('assets.')->group(function () {
-//             Route::get('/', [AssetController::class, 'index'])->name('index');
-//             Route::post('/', [AssetController::class, 'store'])->name('store');
-//             Route::get('/create', [AssetController::class, 'create'])->name('create');
-//             Route::get('/{asset}', [AssetController::class, 'edit'])->name('edit');
-//             Route::put('/{asset}', [AssetController::class, 'update'])->name('update');
-//             Route::delete('/{asset}', [AssetController::class, 'destroy'])->name('destroy');
-//         });
-
-//         Route::prefix('images')->name('images.')->group(function () {
-//             Route::get('/', [ImageController::class, 'index'])->name('index');
-//             Route::post('/', [ImageController::class, 'store'])->name('store');
-//         });
-
-//     });
-// });
 
 
 Route::middleware('auth')->group(function () {
