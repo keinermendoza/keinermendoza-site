5a5dc2043140520010f4048e9d4374b572adacad
Keiner Mendoza <keinermendoza@gmail.com>
2026-07-14 15:55:34 -0400

    fix: redefine foreign key constrains para anular relação entre projetos e tags ao deletar qualquer uma das duas instancias

---
+++ database/migrations/2026_07_14_195428_table_tags_projects_redefine_forign_key_constrains.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tags_projects', function (Blueprint $table) {
+            $table->dropForeign(['tag_id']);
+            $table->dropForeign(['project_id']);
+
+            $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags')
+                ->cascadeOnDelete();
+
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tags_projects', function (Blueprint $table) {
+            //
+        });
+    }
+};
+
+
